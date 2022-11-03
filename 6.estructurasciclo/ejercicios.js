0cfb50a2f0b3b468d89d5302d9fab92cc81561af
FRANKLIN <fryann26@gmail.com>
2022-11-03 11:09:09 -0500

    clase del jueves

--- 6.estructurasciclo/ejercicios.js
+++ 6.estructurasciclo/ejercicios.js
@@ -1,8 +1,46 @@
 //* BUCLE FOR
 
-for (let i=2; i<10; i+=2){
-  console.log(i)
+for (let i=1; i<=20; i+=1){
+  if ( i % 2 == 0 ){
+    console.log(i)
+  } else {
+    console.log(i)
+  }
+  
 }
+
+let vocales= 'aeiou'
+console.log(vocales.length)
+
+for(  i=0; i< vocales.length; i++){
+  console.log (vocales[i])
+}
+//
+
+let texto= 'hola como estas pta'
+let contador= 0
+
+for(  i=0  ; i < texto.length ; i++){
+  if (texto[i]== 'a'){
+    contador+=1
+  }
+}
+console.log('tiene', contador, 'vocales')
+
+/////////////////////////////////////
+ 
+let texto1= 'holaa como estas '
+let contador1= 0
+
+
+
+for(  i=0  ; i < texto1.length ; i++){
+  switch(texto1[i]){
+   case "a": case "e": case "i": case "o": case "u":  
+ contador1+=1
+
+}}
+console.log(contador1)
 
 //* BUCLE WHILE
 
@@ -22,3 +60,7 @@ do {
   i = i + 1;
   console.log(i);
 } while (i < 5);
+
+// con la estructura de for 
+// mostrar los numeros pares entre 1 y 20
+
