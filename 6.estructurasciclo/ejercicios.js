fe9f6f8ace06a58178ce7792df5a54dc30479533
FRANKLIN <fryann26@gmail.com>
2022-11-02 22:18:23 -0500

    tarea de ciclos

---
+++ 6.estructurasciclo/ejercicios.js
@@ -0,0 +1,24 @@
+//* BUCLE FOR
+
+for (let i=2; i<10; i+=2){
+  console.log(i)
+}
+
+//* BUCLE WHILE
+
+let eval = true
+let eval2 = 'salir'
+while (eval == true){
+   if (eval2 == 'salir') {
+       console.log('adios')
+       eval=false
+   }
+}
+
+// BUCLE DO-WHILE
+
+var i = 0;
+do {
+  i = i + 1;
+  console.log(i);
+} while (i < 5);
--- 6.estructurasciclo/index.js
+++ 6.estructurasciclo/index.js
@@ -1,13 +1,45 @@
-//* for 
-// for (let i=2; i<10; i+=2){
-  //  console.log(i)
-//}
- //wile
- let eval = true
- let eval2 = 'salir'
- while (eval == true){
-    if (eval2 == 'salir') {
-        console.log('adios')
-        eval=false
-    }
- }
+
+
+ //* BUCLE FOR
+ //* se vende en la tienda a 12 soles (barras xd )
+ //* El bucle for sirve para ejecutar un código un número conocido de veces, por ejemplo recorrer un array o 
+ //* cualquier otro tipo de colección o simplemente ejecutar el código un número concreto de veces.
+//* El más habitual es en el que se establece el numero desde el que va a empezar el bucle, hasta cuando se
+//* va a seguir ejecutando el bucle y finalmente el modo en el que se actualiza el contador del bucle, que
+//* aunque habitualmente lo que se hace es incrementar en una unidad el contador esto no tiene que ser obligatoriamente así.
+
+//*for (inicializacion; condicion; actualizacion) {
+ //* ...
+/**/// */}
+
+
+
+ //*BUCLE WHILE
+ //*La declaración del bucle comienza con la palabra while seguida de una condición entre paréntesis, 
+ //*donde la condición es una expresión con un resultado verdadero o falso y determina si las instrucciones 
+ //*internas se cumplen o se pasa a la siguiente ejecución del código, luego se colocan las {} y dentro de 
+ //*ellas las instrucciones a ejecutar mientras la condición se cumpla, aquí un pequeño ejemplo para graficar:
+ //* EN EL BUCLE WHILE, LA CONDICIÓN BOOLEANA SE EVALÚA ANTES DE QUE SE EJECUTE EL CÓDIGO
+
+ //* public class EjemploWhile 
+ //*{
+ //* public static void main(String args[]) 
+ //* {
+ //*     int i = 0; //INICIALIZACIÓN DE LA VARIABLE DE CONTROL
+      
+ //*     while (i < 10) 
+ //*     { 
+ //*         //CONDICIÓN A EVALUAR QUE DETERMINARÁ LA CONTINUIDAD O NO DEL BUCLE
+          
+ //*         System.out.println(i);  //EJECUCIÓN DE LAS INSTRUCCIONES DEL CUERPO DEL WHILE
+ //*         i++;  // CONTADOR, VARIABLE ENTERA DE CONTROL
+ //*     }
+ //* }
+ //* }
+
+
+ 
+ //*BUCLE DO WHILE
+ //* en un bucle do … while, la evaluación se hace después de la primera ejecución del ciclo, 
+ //*lo que significa que el bucle obligatoriamente se ejecuta al menos en una ocasión.
+ //* A modo de ejercicio, escribe este código y comprueba los resultados que se obtienen con él:
